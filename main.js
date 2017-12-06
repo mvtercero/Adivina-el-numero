311aeb12fff727bb0f78b4e9f293253f7f8f47b9
Adalaber <mvtercero85@gmail.com>
2017-12-06 17:25:01 +0100

    Add structure HTML and add styles css

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict',
+
+// HISTÓRICO
+
+// <form>
+//   <input id="name" type="text">
+//   <input id="name" type= "submit" value="Guardar nombre">
+//  </form>
+//  <ul>
+//  </ul>
+// <script>
+// var button= document.querySelector ('#button');
+// var input= document.querySelector ('#name');
+// var list= document.querySelector('ul');
+// button.addEventListener ('click', function (event)){
+// event.preventDefault ();
+// var li = document.createElement ('li');
+// li.textContent = input.value;
+// list.appendChild(li);
+// input.value= '';
+// });
+// </script>
